5afaae3add966f9d734a911f4eae629de0af7d9c
kielkow <matheuskiel@fiorifer.com.br>
2023-06-20 10:34:30 -0300

    test: should be able create a meal

---
+++ test/meals.spec.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import { execSync } from 'child_process'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+
+import { app } from '../src/app'
+
+describe('MEALS ROUTES', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able create a meal', async () => {
+    const createUserResponse = await request(app.server).post('/users').send({
+      name: 'jonh doe',
+    })
+
+    const sessionId = createUserResponse.get('Set-Cookie')
+
+    const { statusCode } = await request(app.server)
+      .post('/meals')
+      .send({
+        name: 'lunch',
+        description: 'rice and chicken',
+        date: new Date().toISOString(),
+        respect_diet: true,
+      })
+      .set('Cookie', sessionId)
+
+    expect(statusCode).toEqual(201)
+  })
+})
