cd9f5e40dc3845b30cc94905795118e934ddf452
kielkow <matheuskiel@fiorifer.com.br>
2023-06-26 09:20:54 -0300

    feat: delete meal by ID

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -147,4 +147,35 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     res.status(204)
   })
+
+  app.delete(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (req, res) => {
+      const paramSchema = z.object({ id: z.string().uuid() })
+
+      let id: string
+      try {
+        const params = paramSchema.parse(req.params)
+        id = params.id
+      } catch (error) {
+        return res.status(400).send(JSON.parse(String(error)))
+      }
+
+      const user = await knex('users')
+        .select('*')
+        .where({ session_id: req.cookies.sessionId })
+        .first()
+
+      if (!user) {
+        return res.status(400).send('user with this session_id not found')
+      }
+
+      const options = { id, user_id: user.id }
+
+      await knex('meals').delete().where(options)
+
+      res.status(204)
+    },
+  )
 }
